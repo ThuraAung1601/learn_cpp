9e64b25daaf6b27cf7afb2ed6ac69fef6c2f3b54
June <thuraaung.ai.mdy@gmail.com>
2024-01-03 15:47:44 +0700

    Create no3.cpp

---
+++ lab06/no3.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+int mystery(int, int);
+
+int main() {
+    int x = 0; int y = 0;
+    cout << "Enter two integers: " << endl; cin >> x >> y;
+    cout << "The result is " << mystery(x, y) << endl;
+    return 0;
+}
+
+// int mystery(int a, int b) {
+//     if (b == 1) {
+//         return a;
+//     } else {
+//         // it just muliply a with b
+//         // - at each call a is added to a and go on 
+//         return a + mystery(a, b-1);
+//     }
+// }
+
+int mystery(int a, int b) {
+    if (b == 1) {
+        return a;
+    } else if (b == 0) {
+        return 0;
+    } else if (b < 0) {
+        b = abs(b);
+        a = -a;
+        return a + mystery(a, b-1); 
+    } 
+    else {
+        // it just muliply a with b
+        // - at each call a is added to a and go on 
+        return a + mystery(a, b-1);
+    }
+}
